faf90ac51f433b4618ad049a6faa3228b1775670
artir <artir.guri@mni.thm.de>
2023-03-08 00:58:47 +0100

    added function to render question and answers

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 
+let questionArr = questions_japan;
 let currentQuestion = 0;
 
-function init(){
-    let questionArr = questions_japan;
-    let questions = document.getElementById('questions');
 
+function init(){
+    let questions = document.getElementById('questions');
     questions.innerHTML = totalQuestionsHtml(questionArr);
+
+    showQuestionAndAnswers();
 }
 
 
-function showQuestion(){
+function showQuestionAndAnswers(){
+    let question = questionArr[currentQuestion];
 
+    document.getElementById('question').innerHTML = question['question'];
+    document.getElementById('answer_1').innerHTML = question['answer_1'];
+    document.getElementById('answer_2').innerHTML = question['answer_2'];
+    document.getElementById('answer_3').innerHTML = question['answer_3'];
+    document.getElementById('answer_4').innerHTML = question['answer_4'];
 }
