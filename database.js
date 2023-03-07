01b276818c1860bacef8adf12b02369cf23018c0
artir <artir.guri@mni.thm.de>
2023-03-08 00:27:24 +0100

    added a local database js about 4 wars

---
+++ database.js
@@ -0,0 +1,161 @@
+let questions_ww1 = [{
+    "question": "Wann begann der Erste Weltkrieg?",
+    "answer_1": "28. Juli 1914",
+    "answer_2": "1. September 1939",
+    "answer_3": "6. Juni 1944",
+    "answer_4": "22. Juni 1941",
+    "right_answer": 1
+}, {
+    "question": "Welches Land trat 1917 auf Seiten der Entente in den Ersten Weltkrieg ein?",
+    "answer_1": "Italien",
+    "answer_2": "USA",
+    "answer_3": "Japan",
+    "answer_4": "China",
+    "right_answer": 2
+}, {
+    "question": "Welche Stadt wurde durch den Ersten Weltkrieg zerstört und ist heute Hauptstadt eines souveränen Staates?",
+    "answer_1": "Prag",
+    "answer_2": "Warschau",
+    "answer_3": "Belgrad",
+    "answer_4": "Sarajevo",
+    "right_answer": 4
+}, {
+    "question": "Welcher Staat trat 1915 auf Seiten der Mittelmächte in den Ersten Weltkrieg ein?",
+    "answer_1": "Bulgarien",
+    "answer_2": "Griechenland",
+    "answer_3": "Rumänien",
+    "answer_4": "Serbien",
+    "right_answer": 1
+}, {
+    "question": "Welcher deutsche General war maßgeblich für den Erfolg der Schlacht von Tannenberg verantwortlich?",
+    "answer_1": "Paul von Hindenburg",
+    "answer_2": "Erich Ludendorff",
+    "answer_3": "Fritz von Lossberg",
+    "answer_4": "August von Mackensen",
+    "right_answer": 1
+}];
+
+
+let questions_ww2 = [
+    {
+      "question": "Wann begann der Zweite Weltkrieg offiziell in Europa?",
+      "answer_1": "1. September 1939",
+      "answer_2": "7. Dezember 1941",
+      "answer_3": "6. Juni 1944",
+      "answer_4": "22. Juni 1941",
+      "right_answer": 1
+    },
+    {
+      "question": "Was war das Ziel der Operation Barbarossa?",
+      "answer_1": "Die Eroberung Frankreichs",
+      "answer_2": "Die Eroberung Großbritanniens",
+      "answer_3": "Die Eroberung der Sowjetunion",
+      "answer_4": "Die Eroberung Afrikas",
+      "right_answer": 3
+    },
+    {
+      "question": "Was war die bedeutendste Seeschlacht des Zweiten Weltkriegs?",
+      "answer_1": "Schlacht im Korallenmeer",
+      "answer_2": "Schlacht um Midway",
+      "answer_3": "Schlacht im Atlantik",
+      "answer_4": "Schlacht bei Guadalcanal",
+      "right_answer": 2
+    },
+    {
+      "question": "Wann und wo endete der Zweite Weltkrieg offiziell in Europa?",
+      "answer_1": "8. Mai 1945 in Berlin",
+      "answer_2": "7. Mai 1945 in Reims",
+      "answer_3": "2. September 1945 in Tokio",
+      "answer_4": "15. August 1945 in Potsdam",
+      "right_answer": 2
+    },
+    {
+      "question": "Welcher Code wurde im Zweiten Weltkrieg von den Alliierten geknackt, was den Verlauf des Krieges beeinflusste?",
+      "answer_1": "Der Enigma-Code",
+      "answer_2": "Der Navajo-Code",
+      "answer_3": "Der Caesar-Code",
+      "answer_4": "Der Morse-Code",
+      "right_answer": 1
+    }
+];
+
+let questions_vietnam = [{
+    "question": "Welches Land führte im Vietnamkrieg Krieg gegen die USA?",
+    "answer_1": "Südkorea",
+    "answer_2": "Japan",
+    "answer_3": "Vietnam",
+    "answer_4": "Nordkorea",
+    "right_answer": 3
+}, {
+    "question": "Welches Ereignis löste den Vietnamkrieg aus?",
+    "answer_1": "Die Invasion Südvietnams durch Nordvietnam",
+    "answer_2": "Die Ermordung von Präsident Kennedy",
+    "answer_3": "Der Sturz des südvietnamesischen Präsidenten Diem",
+    "answer_4": "Die Entführung amerikanischer Soldaten",
+    "right_answer": 1
+}, {
+    "question": "Welches Land unterstützte Nordvietnam im Vietnamkrieg?",
+    "answer_1": "Sowjetunion",
+    "answer_2": "Japan",
+    "answer_3": "China",
+    "answer_4": "Nordkorea",
+    "right_answer": 3
+}, {
+    "question": "Wer war der amerikanische Präsident während des Vietnamkriegs?",
+    "answer_1": "Dwight D. Eisenhower",
+    "answer_2": "John F. Kennedy",
+    "answer_3": "Lyndon B. Johnson",
+    "answer_4": "Richard Nixon",
+    "right_answer": 3
+}, {
+    "question": "Welches Jahr endete der Vietnamkrieg?",
+    "answer_1": "1973",
+    "answer_2": "1968",
+    "answer_3": "1975",
+    "answer_4": "1980",
+    "right_answer": 3
+}];
+
+
+let questions_japan =  [
+    {
+      "question": "Wann begann der 2. Japanisch-Chinesische Krieg?",
+      "answer_1": "1931",
+      "answer_2": "1937",
+      "answer_3": "1941",
+      "answer_4": "1945",
+      "right_answer": 2
+    },
+    {
+      "question": "Welches Ereignis löste den Krieg aus?",
+      "answer_1": "Die Besetzung der Mandschurei durch Japan",
+      "answer_2": "Die Besetzung von Shanghai durch China",
+      "answer_3": "Der Angriff auf Pearl Harbor durch Japan",
+      "answer_4": "Der Einmarsch von japanischen Truppen in Hongkong",
+      "right_answer": 1
+    },
+    {
+      "question": "Welche Länder unterstützten China im Krieg?",
+      "answer_1": "USA und Großbritannien",
+      "answer_2": "Sowjetunion und Frankreich",
+      "answer_3": "Australien und Kanada",
+      "answer_4": "Südkorea und Taiwan",
+      "right_answer": 2
+    },
+    {
+      "question": "Wie wurde der Krieg beendet?",
+      "answer_1": "Durch einen Friedensvertrag zwischen Japan und China",
+      "answer_2": "Durch eine UN-Resolution",
+      "answer_3": "Durch den Abzug der japanischen Truppen aus China",
+      "answer_4": "Durch die Kapitulation Japans im Zweiten Weltkrieg",
+      "right_answer": 4
+    },
+    {
+      "question": "Wie viele Menschen starben während des Krieges?",
+      "answer_1": "Über 1 Million",
+      "answer_2": "Über 5 Millionen",
+      "answer_3": "Über 10 Millionen",
+      "answer_4": "Über 20 Millionen",
+      "right_answer": 3
+    }
+  ];
