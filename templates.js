3422ac1709d58bd92152a633f26a6b31399480a4
artir <artir.guri@mni.thm.de>
2023-03-08 23:06:40 +0100

    pretty much done now

--- templates.js
+++ templates.js
@@ -2,7 +2,9 @@
 function totalQuestionsHtml(questionArr, currentQuestion) {
     return (
     `
+    <div>
     <b>${currentQuestion+1}</b> von <b>${questionArr.length}</b> Fragen
+    </div>
     <button class="btn" disabled id="next-button" onclick="nextQuestion()">Nächste Frage</button>
     `
     );
