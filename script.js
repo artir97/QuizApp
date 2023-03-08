0daffe972cc1ef8cc73e4b6f524622278e00004d
artir <artir.guri@mni.thm.de>
2023-03-08 20:46:09 +0100

    fixed not working buttons

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function answer(selection) {
 
     let idOfRightAnswer = `answer_${question['right_answer']}`;
 
-    if (rightAnswerSelected(selectedQuestionNumber)) {
+    if (rightAnswerSelected(selectedQuestionNumber, question)) {
         correctAnswers++;
         document.getElementById(selection).parentNode.classList.add('bg-success');
         AUDIO_SUCCESS.play();
@@ -73,8 +73,7 @@ function answer(selection) {
     document.getElementById('next-button').disabled = false;
 }
 
-
-function rightAnswerSelected(selectedQuestionNumber){
+function rightAnswerSelected(selectedQuestionNumber, question){
     return selectedQuestionNumber === question['right_answer'];
 }
 
